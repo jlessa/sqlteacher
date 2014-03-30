4037f65ef7ccd7739d75532d417d1c6dfb9eb780
jlessa <jefferson2459@gmail.com>
2014-03-30 16:03:16 -0300

    Adicionando projeto

---
+++ app/models/teste.rb
@@ -0,0 +1,4 @@
+class Teste < ActiveRecord::Base
+  self.table_name = 'teste'
+  attr_accessible :idteste
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sqlteacher::Application.config.secret_token = '2566ab92c2b164069a6c77d154136a173eec64f8859910d993fa06451eec91acb9da83761f8fae3f8afb6e726a32d8dc0b622db907a2c34bfa25f9f590fd9705'
